facd734fd50055ef27965ac8360251b6a0e45b1e
陈光辉 <18210809272@163.com>
2020-06-15 16:20:18 +0800

    翻译

--- text/ViewController.m
+++ text/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Firebase.h>
 
 @interface ViewController ()
 
@@ -19,8 +20,32 @@
 
     self.view.backgroundColor = [UIColor whiteColor];
 
-
+    NSArray *array = @[
+        @"我爱你",
+        @"사랑해",
+        @"Ich liebe Dich",
+        @"あいしてる",
+        @"IK hou van jou",
+        @"Jag lskar dig",
+        @"Szeretlek",
+    ];
+    
+    for (NSString *text in array) {
+        FIRLanguageIdentification *languageID = [[FIRNaturalLanguage naturalLanguage] languageIdentification];
+        [languageID identifyPossibleLanguagesForText:text completion:^(NSArray<FIRIdentifiedLanguage *> * _Nullable identifiedLanguages, NSError * _Nullable error) {
+            FIRIdentifiedLanguage *lange = identifiedLanguages.firstObject;
+            FIRTranslateLanguage language = FIRTranslateLanguageForLanguageCode(lange.languageCode);
+            FIRTranslatorOptions *opations = [[FIRTranslatorOptions alloc] initWithSourceLanguage:language targetLanguage:FIRTranslateLanguageEN];
+            FIRTranslator *translator = [[FIRNaturalLanguage naturalLanguage] translatorWithOptions:opations];
+            [translator downloadModelIfNeededWithCompletion:^(NSError * _Nullable error) {
+                [translator translateText:text completion:^(NSString * _Nullable result, NSError * _Nullable error) {
+                    NSLog(@"****翻译完成:%@", result);
+                }];
+            }];
+        }];
+    }
 }
+
 
 
 @end
