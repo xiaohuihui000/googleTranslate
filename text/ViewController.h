1a460fe1fafe95ec49327070289828e7608348f1
陈光辉 <18210809272@163.com>
2020-06-15 14:40:09 +0800

    Initial Commit

---
+++ text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by 陈光辉 on 2020/6/15.
+//  Copyright © 2020 陈光辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ text/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  text
+//
+//  Created by 陈光辉 on 2020/6/15.
+//  Copyright © 2020 陈光辉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
